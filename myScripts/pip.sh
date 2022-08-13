fd4c928b922011bba156210addecee8b83ab38d5
Thomas Grusz <thomasgrusz@gmail.com>
2022-08-13 08:52:50 +0200

    Initial commit

---
+++ myScripts/pip.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+echo "Please do not use pip without specifying a Python Interpreter, i.e.:"
+echo
+echo "\t/Library/Frameworks/Python.framework/Versions/3.10/bin/python3 -m pip"
+echo
---
+++ myScripts/shacheck.sh
@@ -0,0 +1,53 @@
+#!/usr/local/bin/bash
+# This script requires bash 4.4 or higher
+
+#Check if bash version is 4.4 or higher, exit if not
+MAJOR=$(echo $BASH_VERSION | cut -d "." -f 1)
+MINOR=$(echo $BASH_VERSION | cut -d "." -f 2)
+if [[ $MAJOR -lt 4 ]] && [[ $MINOR -lt 4 ]]; then
+	echo
+	echo "Your version of Bash is $MAJOR.$MINOR, but needs to be 4.4 or higher - Program terminated."
+	echo
+	exit
+fi
+
+# Start Sha256 test
+echo -e "\nSha256 Checksum Test"
+echo -e "--------------------\n"
+
+# Load filenames from current working directory into array
+# The readarray -d feature is the one requiring bash version > 4.4
+# The empty string '' means that the elements are NULL delimited comming from the find command
+readarray -d '' LIST_OF_FILENAMES < <(find * -maxdepth 0 -type f -print0)
+array_size=${#LIST_OF_FILENAMES[@]}
+
+# Select a file
+# The select construct allows easy generation of menus
+# https://www.gnu.org/software/bash/manual/html_node/Conditional-Constructs.html#Conditional-Constructs
+echo -e "Select a file from the current working directory : \n"
+select FILENAME in ${LIST_OF_FILENAMES[@]}; do
+	if [ -z $REPLY ] || [ $REPLY -gt $array_size ]; then
+		echo -e "\nYou picked an invalid option - Programm terminated."
+		exit
+	else
+		echo -e "\nYou picked option $REPLY) - $FILENAME\n"
+		break
+	fi
+done
+
+# Enter the checksum
+read -p "Enter SHA256 checksum : " CHECKSUM
+
+# Perform sha256 checksum test
+DERIVED_CHECKSUM=$(shasum -a 256 $FILENAME | cut -d' ' -f1)
+echo -e "Derived checksum      : $DERIVED_CHECKSUM\n"
+
+if [[ $CHECKSUM == $DERIVED_CHECKSUM ]]; then
+	echo "-------------------------------------"
+	echo "OK - Test successful!"
+	echo "====================================="
+else
+	echo "------------------------------------------"
+	echo "ERROR - Sha256 Checksum test did not pass!"
+	echo "=========================================="
+fi
